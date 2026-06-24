d9c856c3f1587e6263591dd75d783b26af916c36
Bhavishya Semwal <semwalb3@gmail.com>
2026-06-25 01:20:36 +0530

    PWA Fix Bugs 5

--- middleware.ts
+++ middleware.ts
@@ -73,6 +73,7 @@ export default clerkMiddleware(async (auth, request) => {
 export const config = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    '/((?!_next|[^?]*\\.(?:html|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)|manifest\\.json|service-worker\\.js).*)',
     "/(api|trpc)(.*)",
     "/__clerk/(.*)",
   ],
