52d29c9fd06e340bb7e1bd5ef987caceb840e205
Bhavishya Semwal <semwalb3@gmail.com>
2026-06-25 06:23:56 +0530

    Fix Bugsss

--- app/page.tsx
+++ app/page.tsx
@@ -93,7 +93,7 @@ function PropertyPreview() {
   useEffect(() => {
     async function fetchFeatured() {
       try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/properties/featured`, {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/properties/featured`, {
           cache: "no-store" 
         });
         
