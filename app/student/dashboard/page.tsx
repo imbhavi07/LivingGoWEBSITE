17e0e75a48535d95ffd59648ab6b67ece551f647
Falit Nautiyal <your@email.comfalitnautiyal7@gmail.com>
2026-07-07 20:22:37 +0530

    directions fix3

--- app/student/dashboard/page.tsx
+++ app/student/dashboard/page.tsx
@@ -52,6 +52,8 @@ export default async function StudentDashboardPage() {
             title: true,
             location: true,
             images: true,
+            lat: true,
+            lng: true,
           }
         }
       },
