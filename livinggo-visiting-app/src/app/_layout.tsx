914f4807944187a9e1068a0c863fa5cb17323f84
Falit25 <falitnautiyal7@gmail.com>
2026-07-16 16:32:09 +0530

    Final App Fix

--- livinggo-visiting-app/src/app/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { StatusBar } from 'react-native';
-
-export default function RootLayout() {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
-    </>
-  );
-}
